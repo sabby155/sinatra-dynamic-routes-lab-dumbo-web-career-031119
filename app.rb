a6b0a74fb1a69a37c927f44f583b52e20255a130
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 21:52:24 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -33,14 +33,13 @@ class App < Sinatra::Base
    @number2 = params[:number2].to_i
    
    if @operation == 'add'
-     "#{@number1 + @number2}"
-
+      "#{@number1 + @number2}"
    elsif @operation == 'multiply'
-     "#{@number1 * @number2}"
+      "#{@number1 * @number2}"
    elsif @operation == 'divide'
-     "#{@number1/@number2}"
+      "#{@number1/@number2}"
    end 
- end 
+
    
  end  
 
