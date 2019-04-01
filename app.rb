182c3b00ecc44edf4a4df79ae9758d5f61de4a9b
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 21:41:06 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -26,7 +26,10 @@ class App < Sinatra::Base
  get '/say/:word1/:word2/:word3/:word4/:word5' do 
   "#{params[:word1]} #{params[:word2]} #{params[:word3]} #{params[:word4]} #{params[:word5]}."
  end 
- get 
+ get '/:operation/:number1/:number2' do 
+   @operation = params[:operation]
+   @number1 = params[:number1]
+ end  
    
 
 end
