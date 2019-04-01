83df3d9a4c512e77562e657752af92dde31ccd69
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 21:48:32 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -32,8 +32,8 @@ class App < Sinatra::Base
    @number1 = params[:number1].to_i
    @number2 = params[:number2].to_i
    
-    (@number1) @operation (@number2)
+    (@number1) (@operation) (@number2)
  end  
- end
+
 
 end
