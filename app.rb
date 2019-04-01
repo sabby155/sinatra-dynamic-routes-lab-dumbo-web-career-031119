30ca929ec8d1b17b1bbeeee95badec335e826d2b
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 21:26:17 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -15,7 +15,7 @@ class App < Sinatra::Base
   
  get '/say/:number/:phrase' do 
    @number = params[:number].to_i
-   @phrase = params[:phrase].to_s
+   @phrase = params[:phrase]
    
    @number.times do |num|
      @phrase
