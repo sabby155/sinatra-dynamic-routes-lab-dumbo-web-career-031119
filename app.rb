e8b8f4169ad8486b6be32fcb7caaa6b4a03f8a64
Sabrina Chowdhury <nebula155@gmail.com>
2019-04-01 21:03:43 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,6 +3,9 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
   
-  
+  get '/reversename/:name'
+    @name = params[:name]
+    "#{@name.reverse}"
+  end 
 
 end
